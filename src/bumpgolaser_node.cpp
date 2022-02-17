3adf256665a2ecbc54a1ba11b61d9a7f664f8c4f
Daniel Alejandro Quinga Lopez <da.quinga.2020@alumnos.urjc.es>
2022-02-17 17:12:28 +0100

    Names changed
    Signed-off-by: Daniel Alejandro Quinga Lopez <da.quinga.2020@alumnos.urjc.es>

--- src/bumpgolaser_node.cpp
+++ src/bumpgolaser_node.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "fsm_bump_go/Laser.h"
+#include "fsm_bump_go/BumpGoLaser.h"
 #include "ros/ros.h"
 
 int main(int argc, char **argv)
--- src/fsm_bump_go/BumpGoLaser.cpp
+++ src/fsm_bump_go/BumpGoLaser.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "fsm_bump_go/Laser.h"
+#include "fsm_bump_go/BumpGoLaser.h"
 #include "ros/ros.h"
 
 namespace fsm_bump_go
